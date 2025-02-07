d2287c2f0c0cb002b7135d68246c71c1bbdeaa2f
luckao.k <lukkao@automotionworks.com>
2025-02-07 14:40:15 +0700

    creating the DBContext

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,3 +1,4 @@
+using GameStore.Api.Data;
 using GameStore.Api.Endpoints;
 using GameStore.Api.Repositories;
 
@@ -5,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddSingleton<IGamesRepository, InMemGamesRepository>();
 
 var connstring = builder.Configuration.GetConnectionString("GameStoreContext");
+builder.Services.AddSqlServer<GameStoreContext>(connstring);
 
 var app = builder.Build();
 
